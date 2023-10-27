707717bd710f1828c910b55341cf01c779eb7a96
E <57803718+id117@users.noreply.github.com>
2023-10-28 00:29:44 +0300

    Create preloaded.py

---
+++ preloaded.py
@@ -0,0 +1,7 @@
+@_test.describe('Check solution size')
+def _solution_check():
+    limit = 55
+    size = __import__('os').stat('/workspace/solution.txt').st_size
+    @_test.it(f'Code size should be less than {limit} chars' + f', actual size: {size}'*(size < limit))
+    def size_test():
+        _test.expect(size < limit, f'Solution too large, actual size: {size}')
