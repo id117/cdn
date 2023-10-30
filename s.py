df492ba0d53943a5dc772dff57b10e3cfce484ce
E <57803718+id117@users.noreply.github.com>
2023-10-30 21:36:56 +0300

    Update s.py

--- s.py
+++ s.py
@@ -2,7 +2,7 @@ prel='''import codewars_test as _test
 @_test.describe('Check solution size')
 def _solution_check():
     limit = 55
-    size = 54
+    size = -256000
     @_test.it(f'Code size should be less than {limit} chars' + f', actual size: {size}'*(size < limit))
     def size_test():
         _test.expect(size < limit, f'Solution too large, actual size: {size}')'''
